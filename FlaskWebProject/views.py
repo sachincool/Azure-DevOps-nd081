33b7b5ab311b9bb287608d11a1302b0f454c3c88
Harshit Luthra <love.coolsachin@gmail.com>
2020-07-04 02:17:44 +0530

    Let's try this

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -111,17 +111,23 @@ def logout():
     return redirect(url_for('login'))
 
 def _load_cache():
-    # TODO: Load the cache from `msal`, if it exists
-    cache = None
+    cache = msal.SerializableTokenCache()
+    if session.get("token_cache"):
+        cache.deserialize(session["token_cache"])
     return cache
 
 def _save_cache(cache):
-    # TODO: Save the cache, if it has changed
+    if cache.has_state_changed:
+        session["token_cache"] = cache.serialize()
 
 def _build_msal_app(cache=None, authority=None):
-    # TODO: Return a ConfidentialClientApplication
-    return None
+    return msal.ConfidentialClientApplication(
+    app.config['CLIENT_ID'], authority=authority or app.config['AUTHORITY'],
+    client_credential=app.config['CLIENT_SECRET'], token_cache=cache)
 
 def _build_auth_url(authority=None, scopes=None, state=None):
     # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return None
+    return _build_msal_app(authority=authority).get_authorization_request_url(
+     scopes or [],
+        state=state or str(uuid.uuid4()),
+        redirect_uri=url_for("authorized", _external=True))
